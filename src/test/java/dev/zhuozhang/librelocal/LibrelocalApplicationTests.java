1802a2b3ebb36e9659111453cb0b8dde33eef84b
Zhuo Zhang <zhuozhang90@gmail.com>
2020-08-01 13:51:41 -0400

    Initial commit

---
+++ src/test/java/dev/zhuozhang/librelocal/LibrelocalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zhuozhang.librelocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrelocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
